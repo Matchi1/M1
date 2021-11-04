822363032cf1039377c90d6f36e60f812b48a540
Chan Mathieu <pro.matchi1@gmail.com>
2021-11-04 23:35:01 +0100

    DESIGN_PATTERN/CmdLineParser: Update Test

--- semestre_1/DESIGN_PATTERN/CmdLineParser/src/fr/uge/poo/cmdline/ex2/CmdLineParserTest.java
+++ semestre_1/DESIGN_PATTERN/CmdLineParser/src/fr/uge/poo/cmdline/ex2/CmdLineParserTest.java
@@ -2,8 +2,9 @@ package fr.uge.poo.cmdline.ex2;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SuppressWarnings("static-method")
 class CmdLineParserTest {
@@ -56,6 +57,48 @@ class CmdLineParserTest {
         var files = parser.process(arguments);
         for(var i = 0; i < files.size(); i++){
             assertEquals(files.get(i), expectedFiles[i]);
+        }
+    }
+
+    @Test
+    public void runnableCorrectlyExecuted() {
+        var parser = new CmdLineParser();
+        var runExecuted = new ArrayList<Integer>();
+        String[] arguments = {"filename1", "-opt", "filename2", "hello", "-legacy"};
+        parser.addFlag("-legacy", () -> runExecuted.add(0));
+        parser.addFlagWithParameter("-opt", argument -> runExecuted.add(1));
+        parser.process(arguments);
+        assertEquals(runExecuted.size(), 2);
+    }
+
+    @Test
+    public void runnableExecutionOrder() {
+        var parser = new CmdLineParser();
+        var runOrder = new ArrayList<Integer>();
+        String[] arguments = {"filename1", "-opt", "filename2", "hello", "-legacy"};
+        Integer[] expectedRunOrder = {1, 0};
+        parser.addFlag("-legacy", () -> runOrder.add(0));
+        parser.addFlagWithParameter("-opt", argument -> runOrder.add(1));
+        parser.process(arguments);
+        assertEquals(runOrder.size(), expectedRunOrder.length);
+        for(var i = 0; i < runOrder.size(); i++){
+            assertEquals(runOrder.get(i), expectedRunOrder[i]);
+        }
+    }
+
+    @Test
+    public void parameterSaved() {
+        var parser = new CmdLineParser();
+        var runOrder = new ArrayList<String>();
+        String[] arguments = {"filename1", "-opt", "filename2", "-opt1", "hello", "-legacy"};
+        String[] expectedParameters = {"filename2", "hello"};
+        parser.addFlag("-legacy", () -> {});
+        parser.addFlagWithParameter("-opt", runOrder::add);
+        parser.addFlagWithParameter("-opt1", runOrder::add);
+        parser.process(arguments);
+        assertEquals(runOrder.size(), expectedParameters.length);
+        for(var i = 0; i < runOrder.size(); i++){
+            assertEquals(runOrder.get(i), expectedParameters[i]);
         }
     }
 }
