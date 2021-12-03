02a96e4542e4b0ac3fdbb8c044829327385c8ff5
CHAN MATHIEU <pro.matchi1@gmail.com>
2021-12-03 15:07:35 +0100

    JAVA/TP10: Sequence

---
+++ semestre_1/JAVA/TP10/src/fr/umlv/seq/Seq.java
@@ -0,0 +1,61 @@
+package fr.umlv.seq;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Seq <E> {
+	private final List<Object> liste;
+	private final Function<? super Object, ? extends E> mapping;
+	
+	private Seq(List<Object> elements, Function<? super Object, ? extends E> mapping) {
+		this.liste = elements;
+		this.mapping = mapping;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Seq(List<? extends E> elements) {
+		this(List.copyOf(elements), o -> (E)o);
+	}
+	
+	public static <T> Seq<T> from(List<? extends T> elements) {
+		return new Seq<>(elements);
+	}
+	
+	public Object get(int index) {
+		return mapping.apply(liste.get(index));
+	}
+	
+	public int size() {
+		return liste.size();
+	}
+	
+	public static <T> Seq<T> of(T... elements) {
+		var temporary = Arrays.asList(elements);
+		return new Seq<>(temporary);
+	}
+	
+	public void forEach(Consumer<? super E> consumer) {
+		Objects.requireNonNull(consumer);
+		liste.forEach(element -> consumer.accept(mapping.apply(element)));
+	}
+	
+	public <T> Seq<T> map(Function<? super E, ? extends T> function) {
+		return new Seq<>(liste, function.compose(mapping));
+	}
+	
+	public Optional<E> findFirst() {
+		return liste.stream().<E>map(mapping).findFirst();
+	}
+	
+	@Override
+	public String toString() {
+		var joiner = new StringJoiner(", ", "<", ">");
+		liste.forEach(element -> joiner.add(mapping.apply(element).toString()));
+		return joiner.toString();
+	}
+}
